c2079737e96698e4ea69402d647439421811f9cf
Ecnac <101355118+Ecnac@users.noreply.github.com>
2022-08-14 18:04:18 +0200

    Début de création du panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,10 @@
+// Création de la page du produit
+
 // Création de la nouvelle URL en utilisant l'ID de l'article
 let url = new URL(window.location.href)
 let id = url.searchParams.get("id")
 console.log(id)
+console.log("Done")
 
 // Déclaration des variables
 const itemImg = document.querySelector(".item__img")
@@ -42,3 +45,54 @@ function colorSelection(color) {
     colorPick.value = color
     colorPick.innerText = color
 }
+
+
+// Création du panier
+
+// Déclaration du panier
+const cart = []
+
+// Récupération de l'élément "quantité"
+const productQuantity = document.getElementById("quantity")
+
+// Récupération de l'élément "couleur"
+const productColor = document.getElementById("colors")
+
+function addToCart () {
+    // Récupération de l'élément "button"
+    const getButton = document.getElementById("addToCart")
+    
+    // Ecoute de l'évenement au clic sur le bouton et vérification de la condition d'ajout au panier
+    getButton.addEventListener("click", function checkSelection () {
+
+        // Récupération du nombre d'articles
+        const pickedQuantity = productQuantity.value
+
+        // Récupération de la couleur de l'article
+        const pickedColor = productColor.value
+
+        // Vérification des conditions d'ajout des articles
+        if (pickedQuantity <= 0 || pickedQuantity > 100 || pickedColor == "") {
+            alert("Veuillez sélectionner une quantité d'articles entre 1 et 100 et/ou une couleur !")
+            productQuantity.value = 1
+            console.log(cart)
+        } else {
+
+            // Création du produit à ajouter au panier
+            const productInfos = {
+                productId : id,
+                quantity : pickedQuantity,
+                color : pickedColor,
+            }
+            
+            // Ajout de l'article au panier
+            cart.push(productInfos)
+            console.log(cart)
+        }
+    })
+}
+
+addToCart()
+
+
+
